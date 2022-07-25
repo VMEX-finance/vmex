12ed4a2586a84c64bbed2858d9419d929eebc6fc
Kevin Yao <ksyao2002@gmail.com>
2022-07-25 11:51:18 -0700

    implement governance function to update risk level of asset, initialize asset risks in test

--- contracts/interfaces/ILendingPool.sol
+++ contracts/interfaces/ILendingPool.sol
@@ -381,6 +381,14 @@ interface ILendingPool {
         uint8 tranche
     ) external;
 
+    /**
+     * @dev Updates the address of the interest rate strategy contract
+     * - Only callable by the LendingPoolConfigurator contract
+     * @param asset The address of the underlying asset of the reserve
+     * @param risk The risk of the asset
+     **/
+    function setAssetRisk(address asset, uint8 risk) external;
+
     function setReserveInterestRateStrategyAddress(
         address reserve,
         uint8 tranche,
