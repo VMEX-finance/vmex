8a3c3e3bc78e67b81406394d306f3d91df3cc2eb
olisa obiora <oobiora22@gmail.com>
2022-08-13 16:51:52 -0400

    constants

--- src.ts/BaseTransactionClass.ts
+++ src.ts/BaseTransactionClass.ts
@@ -1,8 +1,26 @@
-abstract class BaseLendingClass {
-    constructor() {
+import { BigNumberish } from "@ethersproject/bignumber";
+import { EventEmitter } from "events";
+import { Interface } from "@ethersproject/abi";
 
+interface Transaction {
+    chainId: number,
+    to: string,
+    data: any
+}
+
+export abstract class BaseTransactionClass extends EventEmitter {
+    constructor(
+    ) {
+        super()
     }
 
-    abstract createLendingTransaction(): void;
-    abstract submitTransaction(): void;
+    getChainId(): number {
+        return 1 //TODO Implement
+    };
+
+    async submitTransaction(transaction: Transaction): Promise<void> {
+        //TODO implement submitTransaction
+    }
+
 }
+
