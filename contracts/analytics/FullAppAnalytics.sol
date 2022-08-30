066f794e56653242d2c2a99255d2b18dcef16fd5
olisa obiora <oobiora22@gmail.com>
2022-08-30 12:59:20 -0400

    use v2

--- contracts/analytics/FullAppAnalytics.sol
+++ contracts/analytics/FullAppAnalytics.sol
@@ -2,29 +2,28 @@
 pragma solidity >=0.8.0;
 import {AnalyticsUtilities} from "./libraries/AnalyticsUtilities.sol";
 import {Constants} from "./libraries/Constants.sol";
-import {IUiPoolDataProviderV3} from "../misc/interfaces/IUiPoolDataProviderV3.sol";
+import {IUiPoolDataProviderV2} from "../misc/interfaces/IUiPoolDataProviderV2.sol";
 import {ILendingPoolAddressesProvider} from "../interfaces/ILendingPoolAddressesProvider.sol";
 
 struct AggregatedPoolData {
-    IUiPoolDataProviderV3.AggregatedReserveData[] reserveData;
-    IUiPoolDataProviderV3.UserReserveData[] userData;
-    IUiPoolDataProviderV3.BaseCurrencyInfo baseCurrencyInfo;
+    IUiPoolDataProviderV2.AggregatedReserveData[] reserveData;
+    IUiPoolDataProviderV2.UserReserveData[] userData;
+    IUiPoolDataProviderV2.BaseCurrencyInfo baseCurrencyInfo;
     address[] reservesList;
 }
 
 contract FullAppAnalytics {
-    IUiPoolDataProviderV3 dataProvider;
+    IUiPoolDataProviderV2 dataProvider;
 
     constructor(address pool, address user) {
         AggregatedPoolData memory returnData;
-        uint8 integer;
         // scopes, calls to contracts use 9 stack items
         {
             (returnData.reserveData, returnData.baseCurrencyInfo) = dataProvider
                 .getReservesData(ILendingPoolAddressesProvider(pool));
         }
         {
-            (returnData.userData, integer) = dataProvider.getUserReservesData(
+            (returnData.userData) = dataProvider.getUserReservesData(
                 ILendingPoolAddressesProvider(pool),
                 user
             );
