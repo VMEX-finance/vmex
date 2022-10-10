39a9221a04407534189908b54412d55258278bb3
olisa obiora <oobiora22@gmail.com>
2022-10-10 00:31:40 -0400

    +protocol.ts

--- src.ts/protocol.ts
+++ src.ts/protocol.ts
@@ -27,6 +27,95 @@ export async function borrow(params: {
     }
 }
 
+export async function markReserveAsCollateral(params: {
+    signer: ethers.Signer;
+    network: string;
+    asset: string;
+    trancheId: number;
+    useAsCollateral: boolean;    
+}, callback?: () => Promise<any>){
+    const client = await params.signer.getAddress();
+    const lendingPool = await getLendingPoolImpl(params.signer, params.network);
+    await lendingPool.setUserUseReserveAsCollateral(
+        params.asset,
+        params.trancheId,
+        params.useAsCollateral
+    );
+
+    if (callback) {
+        await callback().catch((error) => { console.error("CALLBACK_ERROR: \n", error)});
+    }
+}
+
+export async function withdraw(params: {
+    asset: string;
+    to?: string;
+    trancheId: number;
+    signer: ethers.Signer;
+    interestRateMode: number;
+    referralCode: number;
+    network: string;
+    amount: number | ethers.BigNumberish;
+}, callback?: () => Promise<any>) {
+    let client = await params.signer.getAddress();
+    let to = params.to || client
+    let lendingPool = await getLendingPoolImpl(params.signer, params.network);
+    await lendingPool.withdraw(
+        params.asset,
+        params.trancheId,
+        params.amount,
+        params.interestRateMode,
+        params.referralCode || 0,
+        client
+    );
+
+    if (callback) {
+        await callback().catch((error) => { console.error("CALLBACK_ERROR: \n", error)})
+    }
+}
+
+export async function repay(params: {
+    asset: string;
+    trancheId: number;
+    signer: ethers.Signer;
+    rateMode: number;
+    amount: number | ethers.BigNumberish;
+    network: string;
+}, callback?: () => Promise<any>) {
+    let client = await params.signer.getAddress();
+    let lendingPool = await getLendingPoolImpl(params.signer, params.network);
+    await lendingPool.repay(
+        params.asset,
+        params.trancheId,
+        params.amount,
+        params.rateMode,
+        client
+    );
+
+    if (callback) {
+        callback().catch((error) => { console.error("CALLBACK_ERROR: \n", error)})
+    }
+}
+
+export async function swapBorrowRateMode(params: {
+    asset: string;
+    trancheId: number;
+    rateMode: number;
+    signer: ethers.Signer;
+    network: string;
+}, callback?: () => Promise<any>) {
+    let lendingPool = await getLendingPoolImpl(params.signer, params.network);
+    await lendingPool.swapBorrowRateMode(
+        params.asset,
+        params.trancheId,
+        params.rateMode
+    );
+
+    if (callback) {
+        callback().catch((error) => { console.error("CALLBACK_ERROR: \n", error)});
+    }
+}
+
 export async function supply(params: {
     underlying: string;
     trancheId: number;
