d6ad121077cdaef35a6eaff6a1c573143042724a
oobiora <oo608@nyu.edu>
2022-11-09 11:38:49 -0500

    + totalTranches()

---
+++ packages/sdk/test/analytics.js
@@ -0,0 +1,11 @@
+const { ethers } =  require("ethers");
+const chai = require("chai");
+const { expect } = require("chai");
+const { solidity } = require("ethereum-waffle");
+chai.use(solidity);
+
+describe("Analytics - unit tests", () => {
+    it("1 -", async () => {
+        
+    })
+})
