f72310e503a5c591c0f75d5c5ee8addf31c422eb
Steven <skyao2002@gmail.com>
2022-11-10 18:15:08 -0600

    add tvl calculations, removed dist dir from git in sdk

--- packages/sdk/src.ts/analytics.ts
+++ packages/sdk/src.ts/analytics.ts
@@ -1,27 +1,115 @@
 import { deployments } from "./constants";
 import "@vmex/contracts/artifacts/contracts/misc/WalletBalanceProvider.sol/WalletBalanceProvider.json";
+// import { getAaveProtocolDataProvider, getAToken, getIErc20 } from "@vmex/contracts/helpers/contracts-getters";
 import { BigNumber, ethers } from "ethers";
-import { getLendingPoolImpl, getUnsignedLP, approveUnderlying } from "./utils";
+import {
+  getLendingPoolImpl,
+  getUnsignedLP,
+  approveUnderlying,
+  getAaveProtocolDataProvider,
+  getAToken,
+  getIErc20,
+} from "./utils";
 
 /**
- * calcProtocolTVL()
+ * PROTOCOL LEVEL ANALYTICS
+ */
+
+/**
+ * getTVL()
  * @params { network: string, test: bool }
  * @returns uint(aTokens, underlying)
- * returns a tuple containing the sum of all aTokens in all pools - reflects total supplied, and the sum of underlying amounts - reflecting total borrowed
+ * returns a tuple containing the sum of the balances of all aTokens in all pools
  */
-export async function calcProtocolTVL(params?: {
+export async function getTVL(
+  params: {
+    numTranches: number;
     network?: string;
-    test: boolean;
-}, callback?: () => Promise<any>) {
-    const _lp = getUnsignedLP({ network: params.network || undefined, test: params.test });
-    
+  },
+  callback?: () => Promise<any>
+) {
+  var tvl = BigNumber.from(0);
+  for (var i = 0; i < params.numTranches; i++) {
+    const trancheTvl = await getTrancheTVL({
+        tranche: i,
+        network: params.network
+    });
+    tvl = tvl.add(trancheTvl);
+  }
+
+  return tvl;
 }
 
+/**
+ * TRANCHE LEVEL ANALYTICS
+ */
+
+
+export async function getTrancheTVL(
+  params: {
+    tranche: number;
+    network?: string;
+  },
+  callback?: () => Promise<any>
+) {
+  // get all vTokens associated with the given tranche
+  const [allvTokens, reserveAddresses] = await getTrancheTokens({
+    tranche: params.tranche,
+    network: params.network
+  });
+
+  var tvl = BigNumber.from(0);
+  for (let i = 0; i < allvTokens.length; i++) {
+    const underlying = await getIErc20({
+        address: reserveAddresses[i]
+    });
+    const bal = await underlying.balanceOf(allvTokens[i]);
+    tvl = tvl.add(bal);
+  }
+
+  return tvl;
+}
+
+export async function getTrancheTokens(
+  params: {
+    tranche: number;
+    network?: string;
+  },
+  callback?: () => Promise<any>
+) {
+  const helperContract = await getAaveProtocolDataProvider({
+    network: params.network
+  });
+
+  const allvTokens = await helperContract.getAllATokens(
+    params.tranche.toString()
+  );
+
+  let vTokenAddresses = [];
+  allvTokens.map((vTokenData) => {
+    vTokenAddresses.push(vTokenData.tokenAddress);
+  });
+
+  let underlyingAddresses = [];
+  for (const vTokenAddress of vTokenAddresses) {
+    const vToken = await getAToken({
+        address: vTokenAddress
+    });
+    const underlying = await vToken.UNDERLYING_ASSET_ADDRESS();
+    underlyingAddresses.push(underlying);
+  }
+
+  return [vTokenAddresses, underlyingAddresses];
+}
+
+/**
+ * USER LEVEL ANALYTICS
+ */
 
 /**
  * userAggregatedTrancheData
  * @params { signer: ethers.Signer, tranche: number, network?: string }
- * @returns Promise { 
+ * @returns Promise {
  *  totalCollateralETH: BigNumber
  *  totalDebtETH: BigNumber
  *  availableBorrowsETH: BigNumber
@@ -30,14 +118,23 @@ export async function calcProtocolTVL(params?: {
  *  healthFactor: BigNumber
  * }
  */
-export async function userAggregatedTrancheData( params: { 
+export async function userAggregatedTrancheData(
+  params: {
     signer: ethers.Signer;
     tranche: number;
     network?: string;
     test?: boolean;
-}, callback?: () => Promise<any>) {
-    const lendingPool = await getLendingPoolImpl(params.signer, params.network || "mainnet");
-    return await lendingPool.getUserAccountData(await params.signer.getAddress(), params.tranche);
+  },
+  callback?: () => Promise<any>
+) {
+  const lendingPool = await getLendingPoolImpl(
+    params.signer,
+    params.network || "mainnet"
+  );
+  return await lendingPool.getUserAccountData(
+    await params.signer.getAddress(),
+    params.tranche
+  );
 }
 
 /**
@@ -45,19 +142,35 @@ export async function userAggregatedTrancheData( params: {
  * @params { signer: ethers.Signer, tranche: number, network?: string }
  * @returns tuple(address, uint256)[];
  */
-export async function userTrancheBalances( params: {
-    signer: ethers.Signer,
-    tranche: number,
-    network?: string
-}, callback?: () => Promise<any> ) {
-    const provider = deployments.LendingPoolAddressesProvider[`${params.network || "localhost"}`].address;
-    const _balanceProviderAddress = deployments.WalletBalanceProvider[`${params.network || "localhost"}`].address;
-    const { abi } = require("@vmex/contracts/artifacts/contracts/misc/WalletBalanceProvider.sol/WalletBalanceProvider.json");
+export async function userTrancheBalances(
+  params: {
+    signer: ethers.Signer;
+    tranche: number;
+    network?: string;
+  },
+  callback?: () => Promise<any>
+) {
+  const provider =
+    deployments.LendingPoolAddressesProvider[`${params.network || "localhost"}`]
+      .address;
+  const _balanceProviderAddress =
+    deployments.WalletBalanceProvider[`${params.network || "localhost"}`]
+      .address;
+  const {
+    abi,
+  } = require("@vmex/contracts/artifacts/contracts/misc/WalletBalanceProvider.sol/WalletBalanceProvider.json");
 
-    const balanceProvider = new ethers.Contract(_balanceProviderAddress, abi, params.signer);
-    return await balanceProvider.getUserWalletBalances(provider, await params.signer.getAddress(), params.tranche);
+  const balanceProvider = new ethers.Contract(
+    _balanceProviderAddress,
+    abi,
+    params.signer
+  );
+  return await balanceProvider.getUserWalletBalances(
+    provider,
+    await params.signer.getAddress(),
+    params.tranche
+  );
 }
-
 
 /**
  * getUserReserveConfig
@@ -70,7 +183,7 @@ export async function userTrancheBalances( params: {
 // }, callback?: () => Promise<BigNumber>) {
 //     let configurator = await getLendingPoolConfiguratorProxy();
 //     return configurator.totalTranches;
-//     //sum of atoken amounts in all pools (this will reflect total supplied)? Or sum of actual underlying amounts (which will be total supplied - total borrowed). 
+//     //sum of atoken amounts in all pools (this will reflect total supplied)? Or sum of actual underlying amounts (which will be total supplied - total borrowed).
 // }
 
 // export async function totalMarkets(params?: {
@@ -79,11 +192,10 @@ export async function userTrancheBalances( params: {
 // }, callback?: () => Promise<BigNumber>) {
 //     let configurator = await getLendingPoolConfiguratorProxy();
 //     return configurator.totalTranches;
-//     //sum of atoken amounts in all pools (this will reflect total supplied)? Or sum of actual underlying amounts (which will be total supplied - total borrowed). 
+//     //sum of atoken amounts in all pools (this will reflect total supplied)? Or sum of actual underlying amounts (which will be total supplied - total borrowed).
 // }
 
 //tranche level
-
 
 // //user level (querying by wallet address)
 //  export async function userInfo(params: {
@@ -95,7 +207,6 @@ export async function userTrancheBalances( params: {
 // }, callback?: () => Promise<UserReserveData>) {
 //     let lendingPool = await getLendingPool();
 //     let helpersContract = await getAaveProtocolDataProvider();
-    
+
 //     return getUserData(lendingPool, helpersContract, params.underlying, params.trancheId, await params.signer.getAddress() );
 // }
-
--- packages/sdk/test/sdk.js
+++ packages/sdk/test/sdk.js
@@ -13,7 +13,7 @@ const {
     getLendingPoolReservesList,
     getReserveData
 } = require("../dist/utils.js");
-const { 
+const {
     borrow,
     marketReserveAsCollateral,
     withdraw,
@@ -23,11 +23,11 @@ const {
     markReserveAsCollateral
 } = require("../dist/protocol.js");
 const {
-    userAggregatedTrancheData
+    userAggregatedTrancheData, getTVL, getTrancheTVL
 } = require("../dist/analytics.js");
 
 const WETHadd = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-const WETHabi = [
+const IERC20abi = [
     "function allowance(address owner, address spender) external view returns (uint256 remaining)",
     "function approve(address spender, uint256 value) external returns (bool success)",
     "function balanceOf(address owner) external view returns (uint256 balance)",
@@ -47,33 +47,33 @@ const Crv3Crypto = "0xc4AD29ba4B3c580e6D59105FFf484999997675Ff";
 
 const UNISWAP_ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 const UNISWAP_ROUTER_ABI = require("@vmex/contracts/localhost_tests/abis/uniswapAbi.json")
-const USDCadd = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const USDCaddr = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+
+const network = "localhost";
 
 describe("Supply - end-to-end test", () => {
     let provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
     const owner = provider.getSigner();
-    
+
     it("1 - signer should receive 3 WETH so he can transact for LP tokens", async () => {
-        const WETH = new ethers.Contract(WETHadd, WETHabi, owner);
+        const WETH = new ethers.Contract(WETHadd, IERC20abi, owner);
         await WETH.connect(owner).deposit({ value: ethers.utils.parseEther("3.0")});
         expect(await WETH.balanceOf(await owner.getAddress())).to.be.above(ethers.utils.parseEther("1.0"))
     });
 
     it("2 - should test the approveUnderlying util", async () => {
-        let lendingPool = await getLendingPoolImpl(owner, "localhost");
+        let lendingPool = await getLendingPoolImpl(owner, network);
         await approveUnderlying(owner, ethers.utils.parseEther("3.0"), WETHadd, lendingPool.address);
     })
 
-    
-    
     it("3 - should test lendingPoolSetPause() function", async () => {
         expect(await lendingPoolPause(owner, false, 'localhost', 0)).to.be.false;
-        
+
     })
-    
+
     it("4 - should test whether the lending Pool is paused or not", async () => {
         let lendingPool = await getLendingPoolImpl(owner, 'localhost');
-        expect(await lendingPool.paused(0)).to.be.false;    
+        expect(await lendingPool.paused(0)).to.be.false;
     })
 
     it("5 - should test the protocol supply function", async () => {
@@ -92,11 +92,36 @@ describe("Supply - end-to-end test", () => {
         let { totalCollateralETH } = await userAggregatedTrancheData({
             signer: owner,
             tranche: 0,
-            network: "localhost"
+            network: network
         });
         expect(totalCollateralETH).to.be.above(ethers.utils.parseEther("1.0"));
     })
 
+    it("7 - test that tranche 0 has non zero TVL", async () => {
+        let trancheTvl0 = await getTrancheTVL({
+            tranche: 0,
+            network: network
+        });
+        console.log("tranche 0 tvl is", trancheTvl0.toString());
+        expect(trancheTvl0).to.be.above(ethers.utils.parseEther("1.0"));
+    })
+
+    it("8 - test tranche 1 has zero TVL", async () => {
+        let trancheTvl1 = await getTrancheTVL({
+            tranche: 1,
+            network: network
+        });
+
+        expect(trancheTvl1).to.be.equal(0);
+    })
+
+    it("9 - test that the protocol has non zero TVL", async () => {
+        let protocolTvl = await getTVL({
+            numTranches: 2,
+            network: network
+        });
+        expect(protocolTvl).to.be.above(ethers.utils.parseEther("1.0"));
+    })
 })
 
 describe("Borrow - end-to-end test", () => {
@@ -104,16 +129,16 @@ describe("Borrow - end-to-end test", () => {
     const temp = provider.getSigner(1);
 
     it("1 - should give temp 1 WETH", async () => {
-        const WETH = new ethers.Contract(WETHadd, WETHabi, temp);
+        const WETH = new ethers.Contract(WETHadd, IERC20abi, temp);
         await WETH.connect(temp).deposit({ value: ethers.utils.parseEther("2.0")});
         await WETH.approve(UNISWAP_ROUTER_ADDRESS, ethers.utils.parseEther("1.0"));
         expect(await WETH.balanceOf(await temp.getAddress())).to.be.above(ethers.utils.parseEther("1.0"))
     })
 
     it("2 - should swap for USDC with UNISWAP", async () => {
-        var path = [WETHadd, USDCadd];
+        var path = [WETHadd, USDCaddr];
         var deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
-        const USDC = new ethers.Contract(USDCadd, WETHabi, temp);
+        const USDC = new ethers.Contract(USDCaddr, IERC20abi, temp);
 
         const UNISWAP = new ethers.Contract(UNISWAP_ROUTER_ADDRESS, UNISWAP_ROUTER_ABI, temp);
         await UNISWAP.swapExactETHForTokens(1, path, await temp.getAddress(), deadline, { value: ethers.utils.parseEther("0.5"), gasLimit: "8000000"} );
@@ -122,13 +147,13 @@ describe("Borrow - end-to-end test", () => {
 
 
     it("4 - should supply USDC tokens for aTokens with fn supply()", async () => {
-        USDC = new ethers.Contract(USDCadd, WETHabi, temp)
+        USDC = new ethers.Contract(USDCaddr, IERC20abi, temp)
         expect(await supply({
-            underlying: USDCadd,
+            underlying: USDCaddr,
             trancheId: 0,
             amount: ethers.utils.formatUnits(await USDC.balanceOf(await temp.getAddress())),
             signer: temp,
-            network: "localhost",
+            network: network,
             test: true
         }, () => { return true })).to.be.true;
     })
@@ -136,8 +161,8 @@ describe("Borrow - end-to-end test", () => {
     it("5 - should mark supplied asset for collateral with fn markReserveAsCollateral", async () => {
         markReserveAsCollateral({
             signer: temp,
-            network: "localhost",
-            asset: USDCadd,
+            network: network,
+            asset: USDCaddr,
             trancheId: 0,
             useAsCollateral: true
         }, () => { return true });
@@ -147,19 +172,19 @@ describe("Borrow - end-to-end test", () => {
         let { totalCollateralETH, availableBorrowsETH } = await userAggregatedTrancheData({
             signer: temp,
             tranche: 0,
-            network: "localhost"
+            network: network
         });
         console.log(totalCollateralETH, availableBorrowsETH);
         expect(totalCollateralETH).to.be.above(ethers.utils.parseEther("0"));
     })
 
     it("7 - should test borrowing WETH with borrow() fn", async () => {
-        const WETHContract = new ethers.Contract(WETHadd, WETHabi, temp);
+        const WETHContract = new ethers.Contract(WETHadd, IERC20abi, temp);
         let originalBalance = await WETHContract.balanceOf(await temp.getAddress());
         let { availableBorrowsETH } = await userAggregatedTrancheData({
             signer: temp,
             tranche: 0,
-            network: "localhost"
+            network: network
         })
 
         await borrow({
@@ -168,7 +193,7 @@ describe("Borrow - end-to-end test", () => {
             amount: ethers.utils.parseEther("0.1"),
             interestRateMode: 1,
             signer: temp,
-            network: "localhost",
+            network: network,
             test: true
         })
 
