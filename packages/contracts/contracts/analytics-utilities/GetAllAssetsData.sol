3039e091d9ec8d8e1271893b684a1e5f0adbbf15
Steven <skyao2002@gmail.com>
2022-11-26 11:02:39 -0600

    change back to memory

--- packages/contracts/contracts/analytics-utilities/GetAllAssetsData.sol
+++ packages/contracts/contracts/analytics-utilities/GetAllAssetsData.sol
@@ -14,23 +14,23 @@ contract GetAllAssetsData {
             ILendingPoolAddressesProvider(providerAddr).getLendingPoolConfigurator()
         ).totalTranches();
 
-        // TODO: find deterministic upper bound. temporary solution: 50 assets per tranche
-        QueryAssetHelpers.AssetData[] storage allAssetsData;
-        // uint64 idx = 0;
+        // TODO: find deterministic upper bound. temporary solution: 35 assets per tranche
+        QueryAssetHelpers.AssetData[] memory allAssetsData = new QueryAssetHelpers.AssetData[](totalTranches*35);
+        uint64 idx = 0;
 
         for (uint64 tranche = 0; tranche < totalTranches; tranche++) {
             address[] memory assets = ILendingPool(lendingPool).getReservesList(tranche);
             for (uint64 i = 0; i < assets.length; i++) {
-                allAssetsData.push(QueryAssetHelpers.getAssetData(
-                    assets[i], tranche, providerAddr));
+                allAssetsData[idx++] = QueryAssetHelpers.getAssetData(
+                    assets[i], tranche, providerAddr);
             }
         }
 
-        // QueryAssetHelpers.AssetData[] memory finalData = new QueryAssetHelpers.AssetData[](idx);
-        // for (uint64 i = 0; i < idx; i++) {
-        //     finalData[i] = allAssetsData[i];
-        // }
-	    bytes memory returnData = abi.encode(allAssetsData);
+        QueryAssetHelpers.AssetData[] memory finalData = new QueryAssetHelpers.AssetData[](idx);
+        for (uint64 i = 0; i < idx; i++) {
+            finalData[i] = allAssetsData[i];
+        }
+	    bytes memory returnData = abi.encode(finalData);
         console.log("done with getallassetsdata");
 		assembly {
 			return(add(0x20, returnData), mload(returnData))
