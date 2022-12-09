ccc5d990b83825eafc6b3a637e9cb88061902622
Kevin Yao <ksyao2002@gmail.com>
2022-12-08 18:29:26 -0600

    cleanup emit and events

--- packages/contracts/contracts/interfaces/ILendingPool.sol
+++ packages/contracts/contracts/interfaces/ILendingPool.sol
@@ -37,25 +37,25 @@ interface ILendingPool {
     );
 
     /**
-   * @dev Emitted on borrow() and flashLoan() when debt needs to be opened
-   * @param reserve The address of the underlying asset being borrowed
-   * @param user The address of the user initiating the borrow(), receiving the funds on borrow() or just
-   * initiator of the transaction on flashLoan()
-   * @param onBehalfOf The address that will be getting the debt
-   * @param amount The amount borrowed out
-   * @param borrowRateMode The rate mode: 1 for Stable, 2 for Variable
-   * @param borrowRate The numeric rate at which the user has borrowed
-   * @param referral The referral code used
-   **/
-  event Borrow(
-    address indexed reserve,
-    address user,
-    address indexed onBehalfOf,
-    uint256 amount,
-    uint256 borrowRateMode,
-    uint256 borrowRate,
-    uint16 indexed referral
-  );
+     * @dev Emitted on borrow() and flashLoan() when debt needs to be opened
+     * @param reserve The address of the underlying asset being borrowed
+     * @param user The address of the user initiating the borrow(), receiving the funds on borrow() or just
+     * initiator of the transaction on flashLoan()
+     * @param onBehalfOf The address that will be getting the debt
+     * @param amount The amount borrowed out
+     * @param borrowRateMode The rate mode: 1 for Stable, 2 for Variable
+     * @param borrowRate The numeric rate at which the user has borrowed
+     * @param referral The referral code used
+     **/
+    event Borrow(
+        address indexed reserve,
+        address user,
+        address indexed onBehalfOf,
+        uint256 amount,
+        // uint256 borrowRateMode,
+        uint256 borrowRate,
+        uint16 indexed referral
+    );
 
     /**
      * @dev Emitted on repay()
