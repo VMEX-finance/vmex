ba7f57d999b1708374229a071cac776a1cf92962
kungfuflex <judoflexchop@gmail.com>
2022-12-23 12:52:31 -0500

    update

--- packages/contracts/keccak_helper.ts
+++ packages/contracts/keccak_helper.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers';
 
+
 var libPath ="contracts/libraries/logic/ValidationLogic.sol"
 var libName = "ValidationLogic"
 console.log(ethers.utils.solidityKeccak256(['string'], [`${libPath}:${libName}`]).slice(2, 36))
