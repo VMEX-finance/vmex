0d80c5a613778c2b955b60c0b6b0fc77c5ca0b69
Kevin Yao <ksyao2002@gmail.com>
2023-02-10 17:16:29 -0600

    helper to upgrade configurator

---
+++ packages/contracts/tasks/helpers/upgrade-configurator.ts
@@ -0,0 +1,37 @@
+import { task } from 'hardhat/config';
+import { checkVerification } from '../../helpers/etherscan-verification';
+import { ConfigNames } from '../../helpers/configuration';
+import { printContracts, waitForTx } from '../../helpers/misc-utils';
+import { getEthersSigners, insertContractAddressInDb } from '../../helpers/contracts-helpers';
+import { buildTestEnv, deployLendingPoolConfigurator } from '../../helpers/contracts-deployments';
+import { getLendingPoolAddressesProvider, getLendingPoolConfiguratorProxy } from '../../helpers/contracts-getters';
+import { eContractid } from '../../helpers/types';
+
+task('vmex:upgrade-configurator', 'Deploy development enviroment')
+  .addFlag('verify', 'Verify contracts at Etherscan')
+  .setAction(async ({ verify }, localBRE) => {
+    const POOL_NAME = ConfigNames.Aave;
+
+    await localBRE.run('set-DRE');
+    // Prevent loss of gas verifying all the needed ENVs for Etherscan verification
+    if (verify) {
+      checkVerification();
+    }
+    
+    const addressesProvider = await getLendingPoolAddressesProvider();
+
+    const newConfiguratorImpl = await deployLendingPoolConfigurator(); 
+    await waitForTx(
+      await addressesProvider.setLendingPoolConfiguratorImpl(
+        newConfiguratorImpl.address
+      )
+    );
+
+    const lendingPoolConfiguratorProxy = await getLendingPoolConfiguratorProxy(
+      await addressesProvider.getLendingPoolConfigurator()
+    );
+    await insertContractAddressInDb(
+      eContractid.LendingPoolConfigurator,
+      lendingPoolConfiguratorProxy.address
+    );
+  });
