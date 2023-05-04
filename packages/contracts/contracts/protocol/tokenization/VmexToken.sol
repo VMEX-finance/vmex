06c50915759e94b4dfda97c01a52c219e2f16869
Steven <skyao2002@gmail.com>
2023-05-04 15:50:22 -0500

    hotfix: revert ERC20 changes and create upgraded ERC20

--- packages/contracts/contracts/protocol/tokenization/VmexToken.sol
+++ packages/contracts/contracts/protocol/tokenization/VmexToken.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import {ERC20} from "../../dependencies/openzeppelin/contracts/ERC20.sol";
+import {ERC20Upgraded} from "../../dependencies/openzeppelin/contracts/ERC20Upgraded.sol";
 import {ERC20Permit} from "../../dependencies/openzeppelin/contracts/governance/ERC20Permit.sol";
 import {ERC20Votes} from "../../dependencies/openzeppelin/contracts/governance/ERC20Votes.sol";
 
-contract VmexToken is ERC20, ERC20Permit, ERC20Votes {
+contract VmexToken is ERC20Upgraded, ERC20Permit, ERC20Votes {
 
-    constructor() ERC20("Vmex Token", "VMEX") ERC20Permit("Vmex Token")
+    constructor() ERC20Upgraded("Vmex Token", "VMEX") ERC20Permit("Vmex Token")
     {
         _mint(msg.sender, 100000000e18); // 100 million Vmex
     }
@@ -16,21 +16,21 @@ contract VmexToken is ERC20, ERC20Permit, ERC20Votes {
 
     function _afterTokenTransfer(address from, address to, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        override(ERC20Upgraded, ERC20Votes)
     {
         super._afterTokenTransfer(from, to, amount);
     }
 
     function _mint(address to, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        override(ERC20Upgraded, ERC20Votes)
     {
         super._mint(to, amount);
     }
 
     function _burn(address account, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        override(ERC20Upgraded, ERC20Votes)
     {
         super._burn(account, amount);
     }
