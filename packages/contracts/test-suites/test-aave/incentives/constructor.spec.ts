ff6e7ee6337b27fb494207642b9a41b6d9691198
Kevin Yao <ksyao2002@gmail.com>
2023-06-28 18:42:41 -0500

    quick fix incentives tests

--- packages/contracts/test-suites/test-aave/incentives/constructor.spec.ts
+++ packages/contracts/test-suites/test-aave/incentives/constructor.spec.ts
@@ -1,20 +1,17 @@
 const { expect } = require('chai');
 
 import { makeSuite, TestEnv } from '../helpers/make-suite';
-import { deployIncentivesController } from '../../../helpers/contracts-deployments';
+import { deployIncentivesController, setupVmexIncentives } from '../../../helpers/contracts-deployments';
 import { RANDOM_ADDRESSES } from '../../../helpers/constants';
 
 makeSuite('AaveIncentivesController constructor tests', (testEnv: TestEnv) => {
   it('should assign correct params', async () => {
     const { addressesProvider } = testEnv;
 
-    const peiEmissionManager = RANDOM_ADDRESSES[1];
     const rewardsVault = RANDOM_ADDRESSES[4];
 
-    const aaveIncentivesController = await deployIncentivesController();
+    const aaveIncentivesController = await setupVmexIncentives(rewardsVault);
+
     await expect(await aaveIncentivesController.REWARDS_VAULT()).to.be.equal(rewardsVault);
-    await expect((await aaveIncentivesController.EMISSION_MANAGER()).toString()).to.be.equal(
-      peiEmissionManager
-    );
   });
 });
