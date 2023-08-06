2d84ded0e688dde27ebe4b81b592e7ee30e43aba
Steven <skyao2002@gmail.com>
2023-08-06 18:40:06 -0500

    added admin scripts

---
+++ packages/sdk/README.md
@@ -0,0 +1,16 @@
+# Vmex Protocol Sdk
+
+## Scripts usage
+
+### set-global-admin
+
+```
+NEW_ADMIN=... yarn scripts:update-global-admin:sepolia
+```
+
+### verify-tranche
+
+```
+TRANCHE_ID=... yarn scripts:verify-tranche:sepolia
+```
+
--- packages/sdk/src.ts/admin.ts
+++ packages/sdk/src.ts/admin.ts
@@ -1,7 +1,8 @@
 import { ethers } from "ethers";
 import {
-  getLendingPoolAddressesProvider,
+  getLendingPoolAddressesProvider, getLendingPoolConfiguratorProxy,
 } from "./contract-getters";
+import { ConfigureCollateralParamsInput } from "./interfaces";
 
 export async function setGlobalAdmin(
   params: {
@@ -21,6 +22,86 @@ export async function setGlobalAdmin(
   });
   const tx = await lendingPoolAddressesProvider.setGlobalAdmin(
     params.newGlobalAdmin
+  );
+
+  if (callback) {
+    await callback();
+  }
+  return tx;
+}
+
+export async function verifyTranche(
+  params: {
+    signer: ethers.Signer;
+    trancheId: number;
+    network: string;
+    test?: boolean;
+    providerRpc?: string;
+  },
+  callback?: () => Promise<any>
+) {
+  const lendingPoolConfigurator = await getLendingPoolConfiguratorProxy({
+    signer: params.signer,
+    network: params.network,
+    test: params.test,
+    providerRpc: params.providerRpc,
+  });
+  const tx = await lendingPoolConfigurator.verifyTranche(
+    params.trancheId
+  );
+
+  if (callback) {
+    await callback();
+  }
+  return tx;
+}
+
+export async function unverifyTranche(
+  params: {
+    signer: ethers.Signer;
+    trancheId: number;
+    network: string;
+    test?: boolean;
+    providerRpc?: string;
+  },
+  callback?: () => Promise<any>
+) {
+  const lendingPoolConfigurator = await getLendingPoolConfiguratorProxy({
+    signer: params.signer,
+    network: params.network,
+    test: params.test,
+    providerRpc: params.providerRpc,
+  });
+  const tx = await lendingPoolConfigurator.unverifyTranche(
+    params.trancheId
+  );
+
+  if (callback) {
+    await callback();
+  }
+  return tx;
+}
+
+export async function batchConfigureCollateralParams(
+  params: {
+    signer: ethers.Signer;
+    input: ConfigureCollateralParamsInput[];
+    trancheId: number;
+    network: string;
+    test?: boolean;
+    providerRpc?: string;
+  },
+  callback?: () => Promise<any>
+) {
+  const lendingPoolConfigurator = await getLendingPoolConfiguratorProxy({
+    signer: params.signer,
+    network: params.network,
+    test: params.test,
+    providerRpc: params.providerRpc,
+  });
+  const tx = await lendingPoolConfigurator.batchConfigureCollateralParams(
+    params.input,
+    params.trancheId
   );
 
   if (callback) {
