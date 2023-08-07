f17271243470f2cf7f31e1489d9e7265b137919c
Steven <skyao2002@gmail.com>
2023-08-06 22:03:42 -0500

    verifed admin can only configure approved assets

--- packages/contracts/test-suites/test-aave/verified-tranche.spec.ts
+++ packages/contracts/test-suites/test-aave/verified-tranche.spec.ts
@@ -17,10 +17,10 @@ const chai = require('chai');
 const { expect } = chai;
 
 makeSuite('Verified tranche admins tests', (testEnv) => {
-  const { AM_ASSET_NOT_ALLOWED, INVALID_TRANCHE, ALREADY_VERIFIED, TRANCHE_ADMIN_NOT_VERIFIED, CALLER_NOT_GLOBAL_ADMIN, CALLER_NOT_TRANCHE_ADMIN, LPC_CALLER_NOT_EMERGENCY_ADMIN_OR_VERIFIED_TRANCHE } = ProtocolErrors;
+  const { AM_ASSET_DOESNT_EXIST, INVALID_TRANCHE, ALREADY_VERIFIED, TRANCHE_ADMIN_NOT_VERIFIED, CALLER_NOT_GLOBAL_ADMIN, CALLER_NOT_TRANCHE_ADMIN, LPC_CALLER_NOT_EMERGENCY_ADMIN_OR_VERIFIED_TRANCHE } = ProtocolErrors;
 
   it('Verify tranche 0', async () => {
-    const { pool, yvTricrypto2, ayvTricrypto2, configurator, addressesProvider, users } = testEnv; 
+    const { pool, yvTricrypto2, ayvTricrypto2, configurator, addressesProvider, users } = testEnv;
     await addressesProvider.setTrancheAdmin(users[2].address, 0);
     await expect(configurator.connect(users[2].signer).verifyTranche(0)).revertedWith(CALLER_NOT_GLOBAL_ADMIN)
     await configurator.verifyTranche(0); //verifying tranche to set custom params
@@ -33,8 +33,25 @@ makeSuite('Verified tranche admins tests', (testEnv) => {
     await expect(configurator.verifyTranche(2)).to.be.revertedWith(INVALID_TRANCHE)
   });
 
+  it('Test changing collateral params on asset that DNE fails', async () => {
+    const { configurator, users } = testEnv;
+    await expect(configurator.connect(users[2].signer).batchConfigureCollateralParams([
+        {
+          underlyingAsset: "0xF29Ff96aaEa6C9A1fBa851f74737f3c069d4f1a9",
+          collateralParams: {
+            baseLTV: "800000000000000000",
+            liquidationThreshold: "825000000000000000",
+            liquidationBonus: "1050000000000000000",
+            borrowFactor: "1010000000000000000"
+          }
+        }
+      ],
+      0
+    )).revertedWith(AM_ASSET_DOESNT_EXIST)
+
+  });
   it('Test changing collateral params', async () => {
-    const { dai, ayvTricrypto2, configurator, users, assetMappings } = testEnv; 
+    const { dai, ayvTricrypto2, configurator, users, assetMappings } = testEnv;
     await expect(configurator.connect(users[3].signer).batchConfigureCollateralParams([
         {
           underlyingAsset: dai.address,
@@ -93,7 +110,7 @@ makeSuite('Verified tranche admins tests', (testEnv) => {
 
 
   it('Add some more assets to verified tranche 0', async () => {
-    const {  assetMappings, yvTricrypto2, configurator, dai, users, pool } = testEnv; 
+    const {  assetMappings, yvTricrypto2, configurator, dai, users, pool } = testEnv;
 
     // await expect(assetMappings.getParams(yvTricrypto2.address, 0)).revertedWith(AM_ASSET_NOT_ALLOWED)
 
@@ -103,7 +120,7 @@ makeSuite('Verified tranche admins tests', (testEnv) => {
           reserveFactor: "100000000000000000",
           canBorrow: false,
           canBeCollateral: true
-      }], 
+      }],
     0)).revertedWith(CALLER_NOT_TRANCHE_ADMIN)
 
 
@@ -121,7 +138,7 @@ makeSuite('Verified tranche admins tests', (testEnv) => {
           reserveFactor: "100000000000000000",
           canBorrow: false,
           canBeCollateral: true
-      }], 
+      }],
     0)
 
     console.log("reserve data: ", await pool.getReserveData(yvTricrypto2.address, 0))
@@ -160,7 +177,7 @@ makeSuite('Verified tranche admins tests', (testEnv) => {
   });
 
   it('change interest rate strategy address', async () => {
-    const { configurator, dai , users, pool } = testEnv; 
+    const { configurator, dai , users, pool } = testEnv;
     //global admin can still set it, it won't affect anything
     // await expect(configurator.setReserveInterestRateStrategyAddress(dai.address, 1, dai.address)).revertedWith(TRANCHE_ADMIN_NOT_VERIFIED);
     await expect(configurator.connect(users[2].signer).setReserveInterestRateStrategyAddress(dai.address, 2, dai.address)).revertedWith(TRANCHE_ADMIN_NOT_VERIFIED);
@@ -172,7 +189,7 @@ makeSuite('Verified tranche admins tests', (testEnv) => {
   });
 
   it('Unverify tranche', async () => {
-    const { configurator, users, pool} = testEnv; 
+    const { configurator, users, pool} = testEnv;
     await expect(configurator.connect(users[3].signer).unverifyTranche(0)).revertedWith(TRANCHE_ADMIN_NOT_VERIFIED);
     await expect(configurator.connect(users[2].signer).unverifyTranche(2)).revertedWith(TRANCHE_ADMIN_NOT_VERIFIED);
     await configurator.connect(users[2].signer).unverifyTranche(0);
@@ -182,7 +199,7 @@ makeSuite('Verified tranche admins tests', (testEnv) => {
 
 
   it('Check that the added assets are back to global admin', async () => {
-    const {  assetMappings, yvTricrypto2, configurator, dai, users, pool } = testEnv; 
+    const {  assetMappings, yvTricrypto2, configurator, dai, users, pool } = testEnv;
 
 
     const dai0 = await assetMappings.getParams(dai.address, 0);
@@ -194,7 +211,7 @@ makeSuite('Verified tranche admins tests', (testEnv) => {
     expect(dai0.liquidationBonus).equal(ethers.utils.parseUnits("10500",14))
     expect(dai0.borrowFactor).equal(ethers.utils.parseUnits("10100",14))
     expect(daiR).equal(expectedInterestAdd)
-    
+
 
     const yv0 = await assetMappings.getParams(yvTricrypto2.address, 0);
     const yvTricrypto2R = await assetMappings.getInterestRateStrategyAddress(yvTricrypto2.address, 0);
