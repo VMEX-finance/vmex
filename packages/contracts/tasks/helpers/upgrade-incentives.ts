e009a24fe6163618fcbff32d6ab6f755de936b13
Kevin Yao <ksyao2002@gmail.com>
2023-08-16 11:06:23 -0500

    upgrade incentives and scripts

---
+++ packages/contracts/tasks/helpers/upgrade-incentives.ts
@@ -0,0 +1,40 @@
+import { task } from 'hardhat/config';
+import { checkVerification } from '../../helpers/etherscan-verification';
+import { ConfigNames } from '../../helpers/configuration';
+import { printContracts, waitForTx } from '../../helpers/misc-utils';
+import { getEthersSigners, insertContractAddressInDb } from '../../helpers/contracts-helpers';
+import { buildTestEnv, deployIncentivesController, deployLendingPoolConfigurator } from '../../helpers/contracts-deployments';
+import { getLendingPoolAddressesProvider, getLendingPoolConfiguratorProxy } from '../../helpers/contracts-getters';
+import { eContractid } from '../../helpers/types';
+
+task('vmex:upgrade-incentives', 'Upgrade incentives controller implementation')
+  .addFlag('verify', 'Verify contracts at Etherscan')
+  .setAction(async ({ verify }, localBRE) => {
+    await localBRE.run('set-DRE');
+    // Prevent loss of gas verifying all the needed ENVs for Etherscan verification
+    if (verify) {
+      checkVerification();
+    }
+    
+    const addressesProvider = await getLendingPoolAddressesProvider();
+
+    const newIncentivesImpl = await deployIncentivesController(verify);
+    await waitForTx(
+      await addressesProvider.setIncentivesController(
+        newIncentivesImpl.address
+      )
+    );
+
+    await insertContractAddressInDb(
+      eContractid.IncentivesControllerImpl,
+      newIncentivesImpl.address
+    );
+
+    const incentivesControllerProxy = await addressesProvider.getIncentivesController()
+    console.log("Proxy should still be the same: ", incentivesControllerProxy)
+    
+    // await insertContractAddressInDb(
+    //   eContractid.IncentivesControllerProxy,
+    //   incentivesControllerProxy.address
+    // );
+  });
