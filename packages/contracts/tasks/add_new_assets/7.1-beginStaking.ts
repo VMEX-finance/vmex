9c06751c8f1ba02224ffe79247fe979d0ed4045c
hudeintres <nodadfat@gmail.com>
2023-11-26 16:24:28 -0600

    fixup begin staking add asset

--- packages/contracts/tasks/add_new_assets/7.1-beginStaking.ts
+++ packages/contracts/tasks/add_new_assets/7.1-beginStaking.ts
@@ -12,6 +12,7 @@ import {
 import { getParamPerNetwork } from "../../helpers/contracts-helpers";
 import { ExternalRewardsAddress, IExternalRewardsAddress, eNetwork } from "../../helpers/types";
 import { waitForTx } from "../../helpers/misc-utils";
+import { ZERO_ADDRESS } from "@vmexfinance/sdk";
 
 task(`add-beginStaking`, `setup staking and begin staking for tranche 0`)
 .addParam('pool', `Pool name to retrieve configuration, supported: ${Object.values(ConfigNames)}`)
@@ -36,32 +37,22 @@ task(`add-beginStaking`, `setup staking and begin staking for tranche 0`)
     if(!stakingContracts || !tokens){
       throw "staking contracts not set"
     }
+    const tokensList = Object.values(tokens)
+    const symbolsList = Object.keys(tokens)
 
-    const addressesProvider = await getLendingPoolAddressesProvider();
-    const admin = await addressesProvider.getGlobalAdmin();
     const incentivesController = await getIncentivesControllerProxy();
-    var signer = await getFirstSigner();
     const lendingPool = await getLendingPool();
 
-    const stakingContractValues = Object.values(stakingContracts)
-    const stakingToProcess: ExternalRewardsAddress[] = []
-    const symbolsToProcess: string[] = []
-    for(let i = 0; i<stakingContractValues.length;i++) {
-      const dat = await incentivesController.stakingTypes(stakingContractValues[i].address);
-      if(!dat || dat==0) {
-        stakingToProcess.push(stakingContractValues[i])
-        symbolsToProcess.push(Object.keys(stakingContracts)[i])
+    for(let i = 0; i<tokensList.length;i++) {
+      const token = await lendingPool.getReserveData(tokensList[i], 0);
+      if(token.aTokenAddress!=ZERO_ADDRESS) {
+        const stakingExists = await incentivesController.getStakingContract(token.aTokenAddress);
+        if(stakingExists==ZERO_ADDRESS) { //staking did not yet begin
+          if(stakingContracts[symbolsList[i]]) {
+            console.log("token.aTokenAddress: ",token.aTokenAddress)
+            console.log("staking contract address for ", symbolsList[i], ": ", stakingContracts[symbolsList[i]])
+          }
+        }
       }
-    }
-
-    console.log("stakingToProcess: ", stakingToProcess)
-    console.log("symbolsToProcess: ", symbolsToProcess)
-
-
-    for(let i = 0; i<stakingToProcess.length;i++) {
-      const token = await lendingPool.getReserveData(tokens[symbolsToProcess[i]], 0);
-
-      console.log("token.aTokenAddress: ",token.aTokenAddress)
-      console.log("stakingToProcess[i].address: ", stakingToProcess[i].address)
     }
   });
