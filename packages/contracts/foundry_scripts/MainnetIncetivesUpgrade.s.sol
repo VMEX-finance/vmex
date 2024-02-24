86d879c397b5e08db51e99eeb2ba8d643ac13b92
Filip Lasic <filip.lasic89@gmail.com>
2024-02-24 21:24:02 +0100

    deployment script

--- packages/contracts/foundry_scripts/MainnetIncetivesUpgrade.s.sol
+++ packages/contracts/foundry_scripts/MainnetIncetivesUpgrade.s.sol
@@ -30,6 +30,12 @@ contract IncentivesUpgradeScript is Script {
     uint256 TOKEN_USDC_DECIMALS = 6;
     address TOKEN_LUSD = 0xc40F949F8a4e094D1b49a23ea9241D289B7b2819;
     address TOKEN_WSTETH = 0x1F32b1c2345538c0c6f582fCB022739c4A194Ebb;
+    address TOKEN_OP_WETH_YEARN = 0xDdDCAeE873f2D9Df0E18a80709ef2B396d4a6EA5;
+    address TOKEN_WETH_YEARN = 0x5B977577Eb8a480f63e11FC615D6753adB8652Ae;
+    address TOKEN_ETH_RETH_AURA = 0x4Fd63966879300caFafBB35D157dC5229278Ed23;
+    address TOKEN_ETH_WSTETH_VELO = 0x6dA98Bde0068d10DDD11b468b197eA97D96F96Bc;
+    address TOKEN_ETH_LUSD_VELO = 0x6387765fFA609aB9A1dA1B16C455548Bfed7CbEA;
+    address TOKEN_USDC_LUSD_VELO = 0xf04458f7B21265b80FC340dE7Ee598e24485c5bB;
 
     uint64 LP_TRANCHE_ID = 0;
     uint64 BASE_TRANCHE_ID = 1;
@@ -42,11 +48,7 @@ contract IncentivesUpgradeScript is Script {
     function run() public {
         vm.startBroadcast(MULTISIG);
 
-        _upgradeIncentivesController();
-
         _addMainnetRewards();
-
-        _updateBoostedBalances();
 
         vm.stopBroadcast();
     }
@@ -73,22 +75,37 @@ contract IncentivesUpgradeScript is Script {
     }
 
     function _addMainnetRewards() internal {
-        IDVmex(DVMEX).approve(address(incentivesController), 500_000e18);
+        IDVmex(DVMEX).mint(MULTISIG, 500_100e18);
+        IDVmex(DVMEX).approve(address(incentivesController), 500_100e18);
 
         // USDC LP tranche
         DataTypes.ReserveData memory reserveData = lendingPool.getReserveData(TOKEN_USDC, LP_TRANCHE_ID);
-        _queueNewRewards(reserveData.aTokenAddress, 175_000e18);
+        _queueNewRewards(reserveData.aTokenAddress, 100_000e18);
 
         // WETH accross base LP & LSD tranches
         reserveData = lendingPool.getReserveData(TOKEN_WETH, LP_TRANCHE_ID);
         _queueNewRewards(reserveData.aTokenAddress, 100_000e18);
 
         reserveData = lendingPool.getReserveData(TOKEN_WETH, LSD_TRANCHE_ID);
-        _queueNewRewards(reserveData.aTokenAddress, 125_000e18);
-
-        // wstETH accross base LP & LSD tranches
-        reserveData = lendingPool.getReserveData(TOKEN_WSTETH, LP_TRANCHE_ID);
         _queueNewRewards(reserveData.aTokenAddress, 100_000e18);
+        
+        reserveData = lendingPool.getReserveData(TOKEN_OP_WETH_YEARN, LP_TRANCHE_ID);
+        _queueNewRewards(reserveData.aTokenAddress, 33_350e18);
+
+        reserveData = lendingPool.getReserveData(TOKEN_WETH_YEARN, LP_TRANCHE_ID);
+        _queueNewRewards(reserveData.aTokenAddress, 33_350e18);
+
+        reserveData = lendingPool.getReserveData(TOKEN_ETH_RETH_AURA, LP_TRANCHE_ID);
+        _queueNewRewards(reserveData.aTokenAddress, 33_350e18);
+
+        reserveData = lendingPool.getReserveData(TOKEN_ETH_WSTETH_VELO, LP_TRANCHE_ID);
+        _queueNewRewards(reserveData.aTokenAddress, 33_350e18);
+
+        reserveData = lendingPool.getReserveData(TOKEN_ETH_LUSD_VELO, LP_TRANCHE_ID);
+        _queueNewRewards(reserveData.aTokenAddress, 33_350e18);
+
+        reserveData = lendingPool.getReserveData(TOKEN_USDC_LUSD_VELO, LP_TRANCHE_ID);
+        _queueNewRewards(reserveData.aTokenAddress, 33_350e18);
     }
 
     function _upgradeIncentivesController() internal {
